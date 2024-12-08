0b48d9340a278f12475a9f095726190ce1551089
Алекх <goltvenkooleg@gmail.com>
2024-12-08 23:27:54 +0300

    My first commit

---
+++ catalog_ui.py
@@ -0,0 +1,8 @@
+import tkinter as tk
+from tkinter import ttk, messagebox
+
+class Product:
+    def __init__(self, name, price, description):
+        self.name = name
+        self.price = price
+        self.description = description
